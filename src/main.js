4281911082b20a98a6de51108f0a594bcad2e86d
Hellwood1 <samburik123@gmail.com>
2024-01-17 21:08:08 +0100

    Upd. v.1.0
    - Створена базова структура проєкту;
    
    - Видалено лишні матеріали;
    
    - Ініційовано підключення бібліотек "iziToast" та "simpleLightbox"
    
    - Створено базову .html розмітку;
    
    - В main.js додано імпорти для підключення CSS бібліотек.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const BASE_URL = 'https://pixabay.com/api';
